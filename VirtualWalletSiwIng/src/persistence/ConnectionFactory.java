bf4179b4271863c65a7b9f98f415d680cdf923da
Guido Scarlato <scarlato.guido98@gmail.com>
2018-12-08 17:19:04 +0100

    Connect servlet to database, now signup form work.

---
+++ VirtualWalletSiwIng/src/persistence/ConnectionFactory.java
@@ -0,0 +1,73 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionFactory {
+	private Connection c;
+	public ConnectionFactory() {
+		
+		System.out.println("Connettendo...");
+		try {
+			 
+			 try {
+				Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/VirtualWallet","postgres","forzamilan98");
+			 System.out.println("Connesso al database");
+			 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+	
+	public void stampaUtenti() {
+		try {
+			Statement s = c.createStatement();
+			ResultSet results=s.executeQuery("SELECT * FROM public.\"Utente\"");
+			
+			while(results.next()) {
+				System.out.println(results.getString("Nome")+" "+results.getString("Cognome"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void inserisciUtente(int ID, String nome, String cognome) {
+		try {
+			Statement s = c.createStatement();
+			
+			s.execute("INSERT INTO public.\"Utente\"(\"ID\",\"Nome\",\"Cognome\") values('"+ID+"','"+nome+"','"+cognome+"');");
+			System.out.println("inserito");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			c.close();
+			System.out.println("chiuso");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+}
---
+++ VirtualWalletSiwIng/src/servlets/SignupServlet.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import persistence.ConnectionFactory;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nome =request.getParameter("nome");
+		String cognome=request.getParameter("cognome");
+		System.out.println(nome);
+		System.out.println(cognome);
+		
+		ConnectionFactory c = new ConnectionFactory();
+		c.inserisciUtente(1, nome, cognome);
+		c.close();
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<p>Benvenuto "+nome+" "+cognome+"</p>");
+	}
+
+}
